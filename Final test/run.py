08172c766c1490ad2e78cf56d009fe5fd8cfedfd
AleksandrNZaitsev <a.zaytcev@inbox.ru>
2026-05-27 22:20:23 +0300

    Update run.py

--- Final test/run.py
+++ Final test/run.py
@@ -13,11 +13,21 @@ def main():
 
     analyzer = OrderAnalyzer(config)
     processed, errors = analyzer.process_all_files()
-
+    print(f"\n{'=' * 50}")
+    print(f"Обработано успешно: {processed}")
+    print(f"Обработано с ошибками: {errors}")
+        # Вывод результатов на экран и в файл results.txt
     if analyzer.results:
         print('\nРЕЗУЛЬТАТЫ ПО ФАЙЛАМ:')
         for r in analyzer.results:
-            print(f"{r['filename']}: {r["orders_count"]} заказов, выручка: {r['revenue']:.2f}")
+            if all(k in r for k in ['filename', 'orders_count','revenue']):
+                print(f"  {r['filename']:20} | Заказов: {r['orders_count']:6} | Выручка: {r['revenue']:12.2f} | Средний чек: {r['avg_order_value']:8.2f}")
+            else:
+                print(f"Неполные данные в результате: {r}")
+    else:
+        print("\n Нет результатов для отображения")
+
+    print("="*50)
 
 if __name__ == '__main__':
     main()
